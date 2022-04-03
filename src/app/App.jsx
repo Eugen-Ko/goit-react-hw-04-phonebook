d2fff20a0f3352e2dd3e9b9c1d99e038c968d523
Eugen Kovalchuk <kovalchuk.eak@gmail.com>
2022-04-03 20:17:04 +0300

    Update App.jsx

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -5,20 +5,21 @@ import Filter from '../components/Filter';
 import Notification from '../components/Notification';
 import {ContainerMain, H1, H5 } from '../Components.styled';
 
+const initialState = [      
+  {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+  {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+  {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+  {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'}
+]
+
 export default function App() {
   const [contacts, setContacts] = useState([]);
   const [filter, setFilter] = useState('');
 
   useEffect( () => {
-      const contact = localStorage.getItem('contacts');
-      const parsedContact = JSON.parse(contact);
+      const parsedContact = JSON.parse(localStorage.getItem('contacts'));
       if (parsedContact) {setContacts(parsedContact)}
-      else {setContacts([      
-        {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-        {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-        {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-        {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'}
-      ])}
+      else {setContacts(initialState)}
     }
   , []);
   
@@ -36,7 +37,7 @@ export default function App() {
       return
     }
     const contact = {id, name, number};
-    setContacts([contact, ...contacts]);
+    setContacts( prev=> [contact, ...contacts]);
     resetForm({e});
   }
 
